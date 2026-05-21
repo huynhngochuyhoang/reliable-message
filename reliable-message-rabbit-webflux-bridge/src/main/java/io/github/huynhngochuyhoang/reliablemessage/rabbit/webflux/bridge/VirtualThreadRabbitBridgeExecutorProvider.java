971957c511aeabe1805d4710153d7b1446ef5acc
hoang.huynh <hoang.huynh@mservice.com.vn>
2026-05-21 21:36:11 +0700

    Update VirtualThreadRabbitBridgeExecutorProvider to use custom ThreadFactory for improved thread naming

--- reliable-message-rabbit-webflux-bridge/src/main/java/io/github/huynhngochuyhoang/reliablemessage/rabbit/webflux/bridge/VirtualThreadRabbitBridgeExecutorProvider.java
+++ reliable-message-rabbit-webflux-bridge/src/main/java/io/github/huynhngochuyhoang/reliablemessage/rabbit/webflux/bridge/VirtualThreadRabbitBridgeExecutorProvider.java
@@ -2,13 +2,18 @@ package io.github.huynhngochuyhoang.reliablemessage.rabbit.webflux.bridge;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class VirtualThreadRabbitBridgeExecutorProvider implements RabbitBridgeExecutorProvider {
 
     private final ExecutorService executor;
 
     public VirtualThreadRabbitBridgeExecutorProvider(RabbitWebFluxBridgeProperties.Bridge bridge) {
-        this.executor = Executors.newVirtualThreadPerTaskExecutor();
+        ThreadFactory threadFactory = Thread.ofVirtual()
+                .name("reliable-message-rabbit-bridge-vt-", 0)
+                .factory();
+
+        this.executor = Executors.newThreadPerTaskExecutor(threadFactory);
     }
 
     @Override
